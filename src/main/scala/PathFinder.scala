12ea0e230f79c2090d8ecb8ca8876e9283eddb43
BeckaL <becka.lelew@kaluza.com>
2021-12-05 14:15:36 +0000

    path finder finds path between two points on a line if shortest path is to go off grid

--- src/main/scala/PathFinder.scala
+++ src/main/scala/PathFinder.scala
@@ -4,14 +4,25 @@ object PathFinder {
   def findShortestPath(from: Coordinate, to: Coordinate, grid: Grid): List[Coordinate] = {
     val _ = grid
     if (from.x == to.x) {
-      pointsOnLine(from.y, to.y).map(Coordinate(from.x, _))
+      pointsOnLine(from.y, to.y, grid.height).map(Coordinate(from.x, _))
     } else if (from.y == to.y) {
-      pointsOnLine(from.x, to.x).map(Coordinate(_, from.y))
+      pointsOnLine(from.x, to.x, grid.width).map(Coordinate(_, from.y))
     } else {
       ???
     }
   }
 
-  private def pointsOnLine(from: Int, to: Int): List[Int] =
-    if (from < to) (from to to).toList else (to to from).toList.reverse
+  private def pointsOnLine(from: Int, to: Int, gridDimensionMax: Int): List[Int] = {
+    val toWithWraparound       = if (to > from) to else to + gridDimensionMax
+    val distanceForwards       = toWithWraparound - from
+    val isQuickerToGoBackwards = distanceForwards > gridDimensionMax / 2
+    val increment              = if (isQuickerToGoBackwards) -1 else 1
+    val numberOfSteps          = if (isQuickerToGoBackwards) gridDimensionMax - distanceForwards else distanceForwards
+    (0 until numberOfSteps + 1).toList.map(i => incrementWithWraparound(from, i * increment, gridDimensionMax))
+  }
+
+  // TODO move somewhere common
+  private def incrementWithWraparound(start: Int, increment: Int, max: Int): Int =
+    // adding max size to start and increment enables handling of negative numbers consistently with modulo -> i.e. -1 % 4 becomes 3, not -1
+    (start + increment + max) % max
 }
--- src/test/scala/PathFinderTest.scala
+++ src/test/scala/PathFinderTest.scala
@@ -6,20 +6,45 @@ class PathFinderTest extends FreeSpec with Matchers with TableDrivenPropertyChec
   "PathFinder.findShortestPath" - {
     "should find shortest path when start coord and end coord form a line and quickest route is to stay within grid" in {
       val grid       = Grid(height = 6, width = 6)
-      val startCoord = Coordinate(x = 3, y = 3)
+      val startCoord = (3, 3)
 
       val data = Table(
-        ("targetCoord", "expectedCoordinatesBetweenStartAndTarget"),
-        (Coordinate(3, 5), List(Coordinate(3, 4))), // target is on the same row, reached by incrementing x by 1 each step
-        (Coordinate(5, 3), List(Coordinate(4, 3))), // target is on the same column, reached by incrementing y by 1 each step
-        (Coordinate(1, 3), List(Coordinate(2, 3))), // target is on the same row,  reached by incrementing x by -1 each step
-        (Coordinate(3, 1), List(Coordinate(3, 2)))  // target is on the same column,  reached by incrementing y by -1 each step
+        ("targetCoord", "expectedPath"),
+        ((3, 5), List(startCoord, (3, 4), (3, 5))), // target is on the same row, reached by incrementing x by 1 each step
+        ((5, 3), List(startCoord, (4, 3), (5, 3))), // target is on the same column, reached by incrementing y by 1 each step
+        ((1, 3), List(startCoord, (2, 3), (1, 3))), // target is on the same row,  reached by incrementing x by -1 each step
+        ((3, 1), List(startCoord, (3, 2), (3, 1)))  // target is on the same column,  reached by incrementing y by -1 each step
       )
 
-      forAll(data) { case (targetCoord, expectedCoordinatesBetweenStartAndTarget) =>
-        val expectedPath = startCoord +: expectedCoordinatesBetweenStartAndTarget :+ targetCoord
-        PathFinder.findShortestPath(from = startCoord, to = targetCoord, grid = grid) shouldBe expectedPath
+      forAll(data) { case (targetTuple, expectedPath) =>
+        val result = PathFinder.findShortestPath(from = coordFrom(startCoord), to = coordFrom(targetTuple), grid = grid)
+
+        result shouldBe expectedPath.map(coordFrom)
       }
     }
+
+    "should find shortest path when start coord and end coord form a line and quickest route is to go off grid" in {
+      val grid = Grid(height = 6, width = 6)
+      val data = Table(
+        ("start", "target", "expectedPath"),
+        // quickest route is to move off the grid and appear on the other side, incrementing x by + 1
+        ((0, 0), (0, 4), List((0, 0), (0, 5), (0, 4))),
+        // quickest route is to move off the grid and appear on the other side, incrementing x by -1
+        ((0, 4), (0, 0), List((0, 4), (0, 5), (0, 0))),
+        // quickest route is to move off the grid and appear on the other side, incrementing y by + 1
+        ((0, 0), (4, 0), List((0, 0), (5, 0), (4, 0))),
+        // quickest route is to move off the grid and appear on the other side, incrementing y by - 1
+        ((4, 0), (0, 0), List((4, 0), (5, 0), (0, 0)))
+      )
+
+      forAll(data) { case (startTuple, targetTuple, expectedPath) =>
+        val result = PathFinder.findShortestPath(from = coordFrom(startTuple), to = coordFrom(targetTuple), grid = grid)
+
+        result shouldBe expectedPath.map(coordFrom)
+      }
+    }
+
   }
+
+  private def coordFrom(tuple: (Int, Int)) = Coordinate(tuple._1, tuple._2)
 }
